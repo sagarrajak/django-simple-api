c10215d23f8684be9da3826b89b4446647c63db3
sagar <sagarrajak858@gmail.com>
2019-12-03 02:06:50 +0530

    added final view

--- profile_api/urls.py
+++ profile_api/urls.py
@@ -9,5 +9,6 @@ router.register('profile', views.UserProfileViewSet)
 
 urlpatterns = [
   path('hello-view/', views.HelloApiView.as_view()),
+  path('login/', views.UserLoginApiViews.as_view()),
   path('', include(router.urls))
 ]
